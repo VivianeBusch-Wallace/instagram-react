c8664f669232a7905bf093abd812409f383a6cee
viviane <v.buschwallace@yahoo.com>
2021-06-02 16:07:22 +0200

    main structure, created top-nav

---
+++ src/components/top-navigation-area/top-nav.js
@@ -0,0 +1,11 @@
+function MainNav() {
+  return (
+    <div className="top-nav-container">
+      <div className="logo-container"></div>
+      <div className="searchbar-container"></div>
+      <div className="nav-menu-container"></div>
+    </div>
+  );
+}
+
+export default MainNav;
