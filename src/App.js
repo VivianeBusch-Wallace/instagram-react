b6a251758089f2b77fa3d5058884e5f5eda60a0f
viviane <v.buschwallace@yahoo.com>
2021-06-09 16:07:42 +0200

    trying to figure out fontawesome

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
 // imports
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(element, document.body);
+// importing fontawesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, faCheckSquare, faCoffee);
+
 // importing top navigation
 import MainNav from "./components/top-navigation-area/Top-nav";
 // importing Main
@@ -9,6 +20,15 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
+      <div>
+        <FontAwesomeIcon icon="check-square" />
+        Your <FontAwesomeIcon icon="coffee" /> is hot and ready!
+      </div>
+      <FontAwesomeIcon icon="coffee" />
+      <FontAwesomeIcon icon={["fas", "coffee"]} />
+      <FontAwesomeIcon icon={["far", "coffee"]} />
+      <FontAwesomeIcon icon={faCoffee} />
+
       <MainNav />
       <Main />
       <Footer />
--- src/components/top-navigation-area/Top-nav.js
+++ src/components/top-navigation-area/Top-nav.js
@@ -1,8 +1,12 @@
+import React from "react";
+
+const element = <FontAwesomeIcon icon={faCoffee} />;
+
 function MainNav() {
   return (
     <div className="top-nav-container">
-      <div className="logo-container"></div>
-      <div className="searchbar-container"></div>
+      <div className="logo-container">{element}</div>
+      <div className="searchbar-container">This is searchbar</div>
       <div className="nav-menu-container"></div>
     </div>
   );
