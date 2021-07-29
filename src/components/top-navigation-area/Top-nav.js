fc37dd316ab7eb6f9fd836994280dd2b2659a2e6
viviane <v.buschwallace@yahoo.com>
2021-07-29 22:16:00 +0200

    update indicator dot added to nav menu plus some descriptive comments

--- src/components/top-navigation-area/Top-nav.js
+++ src/components/top-navigation-area/Top-nav.js
@@ -19,15 +19,19 @@ function MainNav() {
       <div className="nav-menu-container">
         <a href="#Home" className="home-icon-link">
           <FontAwesomeIcon icon={faHome} className="fi home-icon" />
+          <div className="indicator indi-home" />
         </a>
         <a href="#Messages" className="paper-plane-icon-link">
           <FontAwesomeIcon icon={faPaperPlane} className="fi messages-icon" />
+          <div className="indicator inndi-messages" />
         </a>
         <a href="#Explore" className="compass-icon-link">
           <FontAwesomeIcon icon={faCompass} className="fi explore-icon" />
+          <div className="indicator indi-explore" />
         </a>
         <a href="#AccountActivity" className="heart-icon-link">
           <FontAwesomeIcon icon={faHeart} className="fi activity-icon" />
+          <div className="indicator indi-activity" />
         </a>
         <a href="#User" className="user-profile-pic">
           <img
