c5119f3ce2adb692b53b379e91c3d9c8fe870328
viviane <v.buschwallace@yahoo.com>
2021-08-04 00:27:40 +0200

    main skeleton beginnings

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,7 +1,44 @@
 import React from "react";
 
 function Main() {
-  return <section className="main">Hello, I am the temporary Main.</section>;
+  return (
+    <section className="main">
+      <div className="account-container">
+        <div className="account-profile-pic-container">
+          <img
+            className="account-profile-pic"
+            src="./images/insta-profile-pic.png"
+            alt="account profile image"
+          />
+        </div>
+
+        <div className="account-information-container">
+          <div className="account-name-actions"></div>
+          <div className="account-data"></div>
+          <div className="account-bio"></div>
+        </div>
+
+        <div className="stories-container">
+          <div className="story-container">
+            <img
+              className="story-image"
+              alt="story image"
+              src="./images/insta-profile-pic.png"
+            />
+            <h3 className="story-title">Newest Story</h3>
+          </div>
+          <div className="story-container">
+            <img
+              className="story-image"
+              alt="story image"
+              src="./images/insta-profile-pic.png"
+            />
+            <h3 className="story-title">Nova</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Main;
